b752e02d50877c337e57617b5bbc9266d2050e6e
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-02-07 21:01:02 -0500

    Updated for Operator SDK v0.4.0

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ mkdir roles
 git clone https://github.com/redhat-gpte-devopsautomation/ansible-operator-roles
 cp -R ansible-operator-roles/roles/postgresql-ocp ./roles
 cp -R ansible-operator-roles/roles/sonarqube-ocp ./roles
-cp ansible-operator-roles/playbooks/sonarqube.yaml ./playbook.yaml
+cp ansible-operator-roles/playbooks/sonarqube.yaml ./playbook.yml
 rm -rf ansible-operator-roles
 
 # Now build the Operator
-operator-sdk build quay.io/wkulhanek/sonarqube-operator:v0.0.2
-docker push quay.io/wkulhanek/sonarqube-operator:v0.0.2
+operator-sdk build quay.io/wkulhanek/sonarqube-operator:v0.0.4
+docker push quay.io/wkulhanek/sonarqube-operator:v0.0.4
